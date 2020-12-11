dcb25cc250425ced9818dee8faac28754432974e
megadeth10 <megadeth10@ddingdong.net>
2020-12-11 16:41:41 +0900

    불필요한 data 삭제

--- app/src/main/java/com/example/network/networkConstans.java
+++ app/src/main/java/com/example/network/networkConstans.java
@@ -3,8 +3,6 @@ package com.example.network;
 import com.example.myapplication.BuildConfig;
 
 public class networkConstans {
-//    static public String APP_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImF1dGhJZCI6ImhvbmV5YmVlcyIsInVzZXJJZCI6ImhvbmV5YmVlcyIsInJvbGVzIjpbImFwcHVzZXIiXX0sImlhdCI6MTU0OTkzNTM1MSwiZXhwIjo4NjU1NDk5MzUzNTF9.GDawuSMsTRDxxGwR9jbBp8VOr4EJ_QeW09mty-cNLSM";
-//    static private String BASE_URL = "https://api-dev.honeybees.co.kr";
     static public String APP_TOKEN = BuildConfig.ACCESS_TOKEN;
     static private String BASE_URL = BuildConfig.BASE_URL;
     static public int DEFAULT_TIMEOUT = 1000;
@@ -13,7 +11,7 @@ public class networkConstans {
     static private String USER_URL_PREFIX = "/api-us";
     static private String AUTH_URL_PREFIX = "/api-as";
     static private String COMMON_URL_PREFIX = "/api-cs";
-    static private String NICEPAY_URL_PREFIX = "/api-ps/v1/nicepay/payment?method=";
+    static private String PAY_URL_PREFIX = "/api-ps/v1/nicepay/payment?method=";
     static private String EVENT_URL_PREFIX = "/api-es";
     static private String PAYMENT_URL_PREFIX = "/api-pns";
     static public String BRANCH_URL = networkConstans.BASE_URL +  networkConstans.BRANCH_URL_PREFIX;
@@ -21,7 +19,7 @@ public class networkConstans {
     static public String USER_URL =  networkConstans.BASE_URL +  networkConstans.USER_URL_PREFIX;
     static public String AUTH_URL =  networkConstans.BASE_URL +  networkConstans.AUTH_URL_PREFIX;
     static public String COMMON_URL =  networkConstans.BASE_URL +  networkConstans.COMMON_URL_PREFIX;
-    static public String NICEPAY_URL =  networkConstans.BASE_URL +  networkConstans.NICEPAY_URL_PREFIX;
+    static public String PAY_URL =  networkConstans.BASE_URL +  networkConstans.PAY_URL_PREFIX;
     static public String EVENT_URL =  networkConstans.BASE_URL +  networkConstans.EVENT_URL_PREFIX;
     static public String PAYMENT_URL =  networkConstans.BASE_URL +  networkConstans.PAYMENT_URL_PREFIX;
 }
