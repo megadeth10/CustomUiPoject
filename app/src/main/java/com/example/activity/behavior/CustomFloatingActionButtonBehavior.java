64c824c7184d01af6b467ef18a85bd6e27aa3fdf
megadeth10 <megadeth10@ddingdong.net>
2020-12-23 10:49:08 +0900

    테스트용 SnackBar 관련하여 FAB behavior 작업완료함.

--- app/src/main/java/com/example/activity/behavior/CustomFloatingActionButtonBehavior.java
+++ app/src/main/java/com/example/activity/behavior/CustomFloatingActionButtonBehavior.java
@@ -11,9 +11,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+/**
+ * SnackBar 사용시 FAB behavior 수정파일
+ * 물론 com.google.android.material.floatingactionbutton.FloatingActionButton$Behavior 이것이 있다.
+ */
 public class CustomFloatingActionButtonBehavior
         extends CoordinatorLayout.Behavior<FloatingActionButton> {
     private final String TAG = CustomFloatingActionButtonBehavior.class.getSimpleName();
+
     public CustomFloatingActionButtonBehavior() {
     }
 
@@ -24,6 +29,7 @@ public class CustomFloatingActionButtonBehavior
     /**
      * 레이아웃에 트리거가 발생하면 호출
      * true를 리턴하면 onDependentViewChanged 호출
+     *
      * @param parent
      * @param child
      * @param dependency
@@ -34,7 +40,7 @@ public class CustomFloatingActionButtonBehavior
     public boolean layoutDependsOn(@NonNull CoordinatorLayout parent,
                                    @NonNull FloatingActionButton child,
                                    @NonNull View dependency) {
-        if(dependency instanceof Snackbar.SnackbarLayout) {
+        if (dependency instanceof Snackbar.SnackbarLayout) {
             Log.e(TAG, String.format("layoutDependsOn() %s",
                     dependency.getClass().getSimpleName()));
             return true;
@@ -45,6 +51,7 @@ public class CustomFloatingActionButtonBehavior
 
     /**
      * dependency의 View의 변화가 있을때 이벤트가 들어옴.
+     *
      * @param parent
      * @param child
      * @param dependency
@@ -54,14 +61,31 @@ public class CustomFloatingActionButtonBehavior
     public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent,
                                           @NonNull FloatingActionButton child,
                                           @NonNull View dependency) {
-        if(dependency instanceof Snackbar.SnackbarLayout){
+        if (dependency instanceof Snackbar.SnackbarLayout
+                && child.getTranslationY() == 0) {
             float transitionY = Math.min(0,
                     dependency.getTranslationY() - dependency.getHeight());
             child.setTranslationY(transitionY);
-            Log.e(TAG, String.format("onDependentViewChanged() t: %f, height : %d",
-                    dependency.getTranslationY(), dependency.getHeight()));
+            Log.e(TAG, String.format("onDependentViewChanged() transitionY: %f",
+                    transitionY));
         }
 //        return super.onDependentViewChanged(parent, child, dependency);
         return false;
+    }
+
+    /**
+     * layoutDependsOn에 설정한 View가 제거시
+     * @param parent
+     * @param child
+     * @param dependency
+     */
+    @Override
+    public void onDependentViewRemoved(@NonNull CoordinatorLayout parent, @NonNull FloatingActionButton child, @NonNull View dependency) {
+        super.onDependentViewRemoved(parent, child, dependency);
+        if (dependency instanceof Snackbar.SnackbarLayout){
+            Log.e(TAG, String.format("onDependentViewRemoved() %s",
+                    dependency.getClass().getSimpleName()));
+            child.setTranslationY(0);
+        }
     }
 }
